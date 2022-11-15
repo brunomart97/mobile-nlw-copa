a8f0fe5aeac5fa0e33cafec24f16bbc63bd6c9f5
Bruno Martins <brunomart97@hotmail.com>
2022-11-15 17:49:28 -0300

    SignIn interface

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -7,14 +7,20 @@ import { Button } from '../../components/Button'
 
 export const SignIn = () => {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
+        type="SECUNDARY"
         title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
-        type="SECUNDARY"
+        mt={12}
       />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+        de sua conta.
+      </Text>
     </Center>
   )
 }
